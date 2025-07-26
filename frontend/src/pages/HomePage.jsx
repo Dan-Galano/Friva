f3522788b66741cd6b5139870cabf4d6204c07da
dan-galano <dan.galano.dev@gmail.com>
2025-07-27 00:28:21 +0800

    create initial homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import { toast } from "sonner";
+
+const HomePage = () => {
+  return (
+    <div>
+      This is home <br />
+      <button onClick={() => toast.success("Hello")}>Toast!~</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+};
+
+export default HomePage;
