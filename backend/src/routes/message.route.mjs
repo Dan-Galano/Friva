318548644041244150ca07f23f4db1098fab18ac
dan-galano <dan.galano.dev@gmail.com>
2025-07-26 18:08:42 +0800

    Feat: add message route

---
+++ backend/src/routes/message.route.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+import { protectRoute } from "../middlewares/auth.middleware.mjs";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.mjs";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
+export default router;
