66d2ff9f72b21b555e9250fddbf466c0c7aeebce
dan-galano <dan.galano.dev@gmail.com>
2025-07-27 00:24:19 +0800

    configure cors

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -4,6 +4,7 @@ import { connectDB } from "./lib/db.mjs";
 import authRoutes from "./routes/auth.route.mjs";
 import messageRoutes from "./routes/message.route.mjs";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +14,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
