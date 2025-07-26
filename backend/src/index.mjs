8bd50e1ac81afae128ba34bc2a26e7936d5a02a5
dan-galano <dan.galano.dev@gmail.com>
2025-07-26 18:07:45 +0800

    add messageRoutes

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.mjs";
 import authRoutes from "./routes/auth.route.mjs";
+import messageRoutes from "./routes/message.route.mjs";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
