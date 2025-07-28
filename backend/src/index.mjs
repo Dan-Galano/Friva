3e89ea760fbee5a9fadbad2a35e32ef365a9b1a9
dan-galano <dan.galano.dev@gmail.com>
2025-07-29 00:29:22 +0800

    fixed cors error

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -11,13 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
