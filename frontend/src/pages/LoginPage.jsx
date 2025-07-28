0ba861719fc8cb8673e12815cbf1b5c2cf0a2737
dan-galano <dan.galano.dev@gmail.com>
2025-07-29 01:46:10 +0800

    moved image pattern to left side for consistency

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -40,7 +40,14 @@ const LoginPage = () => {
 
   return (
     <div className="h-screen grid lg:grid-cols-2">
-      {/* Left Side - Form */}
+      {/* Left Side - Image/Pattern */}
+      <AuthImagePattern
+        title={"Welcome back!"}
+        subtitle={
+          "Sign in to continue your conversations and catch up with your messages."
+        }
+      />
+      {/* Right Side - Form */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
           {/* Logo */}
@@ -135,15 +142,7 @@ const LoginPage = () => {
             </p>
           </div>
         </div>
-      </div>
-
-      {/* Right Side - Image/Pattern */}
-      <AuthImagePattern
-        title={"Welcome back!"}
-        subtitle={
-          "Sign in to continue your conversations and catch up with your messages."
-        }
-      />
+      </div>      
     </div>
   );
 };
