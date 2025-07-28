3b4f9fc2b2ebb158cd7e454e66d580ed414e926b
dan-galano <dan.galano.dev@gmail.com>
2025-07-28 19:45:06 +0800

    add login & signup auth static functions in zustand

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { api } from "../lib/axios";
+import { toast } from "sonner";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -19,6 +20,46 @@ export const useAuthStore = create((set) => ({
       console.log("Error in checkAuth", error);
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+
+    try {
+      const response = await api.post("/auth/signup", data);
+      set({ authUser: response.data });
+      toast.success("Account created successfully.");
+    } catch (error) {
+      toast.error("Error in signup function", error.response.data.message);
+      console.log(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const response = await api.post("/auth/login", data);
+      set({ authUser: response.data });
+      toast.success("You're now logged in.");
+    } catch (error) {
+      console.log("Error in login function", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      const response = await api.post("/auth/logout");
+      set({ authUser: null });
+      toast.success(response.data.message);
+    } catch (error) {
+      console.log("Error in logout function", error);
+      toast.error("Logout failed.");
     }
   },
 }));
