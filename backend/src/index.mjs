c71d51f75c0a8acd5976819c172cdb5fab0c6d4c
dan-galano <dan.galano.dev@gmail.com>
2025-07-30 16:02:16 +0800

    implement socket.io in index.mjs

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -5,16 +5,19 @@ import authRoutes from "./routes/auth.route.mjs";
 import messageRoutes from "./routes/message.route.mjs";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.mjs";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}))
+// using socket.io
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +26,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
   connectDB();
 });
