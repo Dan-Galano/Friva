fde7cca30758d74b4a85f8a0662d4724cb4e6514
dan-galano <dan.galano.dev@gmail.com>
2025-07-31 20:54:17 +0800

    ready for production

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -7,9 +7,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.mjs";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // using socket.io
 app.use(
@@ -25,6 +28,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
